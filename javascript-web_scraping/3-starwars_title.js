7142d3dda28b56a7856a096bd3c6d7eb76d9974a
German <gerchureloco@gmail.com>
2024-04-28 22:34:58 -0700

    3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,15 +2,15 @@
 
 const request = require('request');
 
-const movieId = process.argv[1];
-const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${process.argv[1];}`;
 
 request.get(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
     return;
   }
-
+  else
   const movie = JSON.parse(body);
   console.log(movie.title);
 });
