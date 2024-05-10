0ee307e7d3dd193c5ba1f521e92155ae2da45a32
German <gerchureloco@gmail.com>
2024-05-10 14:11:41 -0700

    1f

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,13 +1,16 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filePath = process.argv[2];
-const content = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf-8', (err) => {
+if (process.argv.length !== 4) {
+  console.error('Usage: ./1-writeme.js <file_path> <string_to_write>');
+  process.exit(1);
+}
+const filePath = process.argv[2];
+const str = process.argv[3];
+
+fs.writeFile(filePath, str, 'utf8', (err) => {
   if (err) {
-    console.error(err);
-    return;
+    console.log(err);
   }
-  console.log('The file has been saved!');
 });
