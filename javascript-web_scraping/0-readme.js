53bf951a874cb2f5b027c00e1c00ecebed47d762
German <gerchureloco@gmail.com>
2024-07-08 22:16:54 +0000

    0 scraping fix

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', (err, data) => console.log(err ? err : data));
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
