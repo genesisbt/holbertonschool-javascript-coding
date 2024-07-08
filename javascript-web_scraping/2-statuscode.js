f276c0600f7801e618d397b0acb5278f7044afc2
German <gerchureloco@gmail.com>
2024-07-08 22:53:40 +0000

    2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
+
 const request = require('request');
+
 const url = process.argv[2];
 
-function getStatus (url) {
-  request.get(url, (error, response) => {
-    if (error) {
-      console.error(error);
-      return;
-    }
-    console.log('code:', response.statusCode);
-  });
-}
-
-getStatus(url);
+request(url, function (error, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
