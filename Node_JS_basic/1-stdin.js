b8f7baeb50172d30ab7f6a4253532259c9153ca7
German <gerchureloco@gmail.com>
2024-07-08 21:09:53 +0000

    fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,18 @@
-console.log('Welcome to Holberton School, what is your name?');
-process.stdin.setEncoding('utf-8');
-process.stdin.on('data', (data) => {
-  const name = data.trim();
-  console.log(`Your name is: ${name}`);
-  process.stdin.end();
+const readline = require('readline');
+
+const nline = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-process.stdin.on('end', () => {
+console.log('Welcome to Holberton School, what is your name?');
+
+nline.question('', (name) => {
+  console.log(`Your name is: ${name}`);
+  rl.close();
+});
+
+nline.on('close', () => {
   console.log('This important software is now closing');
+  process.exit(0);
 });
