a5cb79a62dfc47eee95bb98f02cb2324afc47d59
German <gerchureloco@gmail.com>
2024-07-08 21:06:37 +0000

    1 with lint

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.setEncoding('utf-8');
+process.stdin.on('data', (data) => {
+  const name = data.trim();
+  console.log(`Your name is: ${name}`);
+  process.stdin.end();
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
