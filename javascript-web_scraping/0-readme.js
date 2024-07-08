296180b1c69d0451111d939bb4f69ffff1d66870
German <gerchureloco@gmail.com>
2024-07-08 22:36:06 +0000

    0fix?

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
 const fs = require('fs');
 
+if (process.argv.length !== 3) {
+  console.error('Usage: ./0-readme.js <file_path>');
+  process.exit(1);
+}
+
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf8', (err, data) => {
@@ -8,5 +13,6 @@ fs.readFile(filePath, 'utf8', (err, data) => {
     console.error(err);
     return;
   }
+  
   console.log(data);
 });
