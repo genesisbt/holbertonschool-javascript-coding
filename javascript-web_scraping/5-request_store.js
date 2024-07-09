2b5de6bc231f48dd00b855f85a32be09e36f283c
German <gerchureloco@gmail.com>
2024-07-09 23:32:30 +0000

    5 try 2

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,32 +1,20 @@
 #!/usr/bin/node
 
-const fs = require('fs');
 const request = require('request');
+const fs = require('fs');
 
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-if (!url || !filePath) {
-  console.error('Usage: node script.js <url> <file-path>');
-  process.exit(1);
+function getContent(url, filepath) {
+  request(url, { encoding: 'utf8' }, (error, response, body) => {
+    if (error) {
+      console.error('Error fetching webpage:', error);
+    } else {
+      fs.writeFileSync(filepath, body, 'utf-8');
+      console.log(`Webpage content successfully saved to ${filepath}`);
+    }
+  });
 }
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.error('Error fetching webpage:', error);
-    process.exit(1);
-  }
-  
-  if (response.statusCode !== 200) {
-    console.error(`Failed to fetch webpage. Status code: ${response.statusCode}`);
-    process.exit(1);
-  }
-
-  fs.writeFile(filePath, body, 'utf-8', function (err) {
-    if (err) {
-      console.error('Error writing to file:', err);
-      process.exit(1);
-    }
-    console.log(`Webpage content successfully saved to ${filePath}`);
-  });
-});
+getContent(url, filePath);
